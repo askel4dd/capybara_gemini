e43407cb84da95010cb57605259a67d7a1e3f49a
Dmitriy Savin <its2exp@gmail.com>
2016-04-11 18:02:41 +0300

    Setup environment for feature tests.

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ ruby '2.1.5'
 
 gem 'chunky_png'
 gem 'rspec'
+gem 'capybara'
+gem 'selenium-webdriver'
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'capybara_gemini/rspec'
+require 'capybara/spec/spec_helper'
+
+RSpec.configure do |config|
+  Capybara::SpecHelper.configure(config)
+end
